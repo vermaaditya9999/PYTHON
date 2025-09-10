d0c06c83c2ada17e9ce5f09dbf54468c46fb700f
Aditya Verma <vermaaditya7871@gmail.com>
2025-09-10 19:41:39 +0530

    Add files via upload

---
+++ Text Editor.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import filedialog,messagebox
+
+def new_file():
+    text.delete(1.0,tk.END)
+
+def open_file():
+    file_path=filedialog.askopenfilename(defaultextension=".txt",filetypes=[("Text Files","*.txt")])
+    if file_path:
+        with open(file_path,'r')as file:
+            text.delete(1.0,tk.END)
+            text.insert(tk.END,file.read())
+def save_file():
+    file_path=filedialog.asksaveasfilename(defaultextension=".txt",filetypes=[("Text Files","*.txt")])
+    if file_path:
+        with open(file_path,'w')as file:
+            file.write(text.get(1.0,tk.END))
+            messagebox.showinfo("info","File saves successfully!")
+
+root=tk.Tk()
+root.title("MY  Text EDITOR ")
+root.geometry("800x600")
+
+menu=tk.Menu(root)
+root.config(menu=menu)
+file_menu=tk.Menu(menu)
+menu.add_cascade(label="File",menu=file_menu)
+file_menu.add_command(label="New ",command=new_file)
+file_menu.add_command(label="open ",command=open_file)
+file_menu.add_command(label="save",command= save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit",command=root.quit)
+
+
+text=tk.Text(root,wrap=tk.WORD,font=("Helvetica",12),fg="black")
+text.pack(expand=tk.YES,fill=tk.BOTH)
+
+root.mainloop()
